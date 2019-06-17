eaae0794164146ba478cb53da1a53695c9ffce30
fermyth <ferry@tagsforfun.com>
2019-06-17 16:58:57 +0700

    Initial Set up Firebase

--- android/app/src/main/java/com/testlaska/MainApplication.java
+++ android/app/src/main/java/com/testlaska/MainApplication.java
@@ -3,6 +3,7 @@ package com.testlaska;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
